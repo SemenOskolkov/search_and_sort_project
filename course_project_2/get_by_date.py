49722750598cd761533a25320a1f8a204d06092d
Semen Oskolkov <oskolkovsemen@yandex.ru>
2022-12-25 18:15:44 +0500

    Changed and optimization code

--- course_project_2/get_by_date.py
+++ course_project_2/get_by_date.py
@@ -10,9 +10,6 @@ def cache(func):
                 read = csv.DictReader(file)
                 result = [*read]
 
-            with open(data/filename, 'w', newline='') as write_file:  # Передает содержимое из файла с кэшем в назначенный файл
-                write_file.writelines(result)
-
         else:
             result = func(*args, **kwargs)
 
@@ -24,8 +21,8 @@ def cache(func):
     return wrapper
 
 
-def binary_search_iterative_first_data(array,
-                                       element):  # Бинарный поиск индекса первого вхождения элемента в списке по дате
+def binary_search_iterative_first(array,
+                                       element, dict_key):  # Бинарный поиск индекса первого вхождения элемента в списке по ключу
     first = 0
     last = len(array)
     first_index = -1
@@ -33,19 +30,19 @@ def binary_search_iterative_first_data(array,
     while (first <= last):
         mid = (first + last) // 2
 
-        if element == array[mid]['date']:
+        if element == array[mid][dict_key]:
             first_index = mid
             last = mid - 1  # Поиск влево (нижние индексы)
 
-        if element < array[mid]['date']:
+        if element < array[mid][dict_key]:
             last = mid - 1
         else:
             first = mid + 1
     return first_index
 
 
-def binary_search_iterative_last_data(array,
-                                      element):  # Бинарный поиск индекса последнего вхождения элемента в списке по дате
+def binary_search_iterative_last(array,
+                                      element, dict_key):  # Бинарный поиск индекса последнего вхождения элемента в списке по ключу
     first = 0
     last = len(array)
     last_index = -1
@@ -53,51 +50,11 @@ def binary_search_iterative_last_data(array,
     while (first <= last):
         mid = (first + last) // 2
 
-        if element == array[mid]['date']:
+        if element == array[mid][dict_key]:
             last_index = mid
             first = mid + 1  # Поиск вправо (более высокие индексы)
 
-        if element < array[mid]['date']:
-            last = mid - 1
-        else:
-            first = mid + 1
-    return last_index
-
-
-def binary_search_iterative_first_name(array,
-                                       element):  # Бинарный поиск индекса первого вхождения элемента в списке по имени
-    first = 0
-    last = len(array)
-    first_index = -1
-
-    while (first <= last):
-        mid = (first + last) // 2
-
-        if element == array[mid]['Name']:
-            first_index = mid
-            last = mid - 1  # Поиск влево (нижние индексы)
-
-        if element < array[mid]['Name']:
-            last = mid - 1
-        else:
-            first = mid + 1
-    return first_index
-
-
-def binary_search_iterative_last_name(array,
-                                      element):  # Бинарный поиск индекса последнего вхождения элемента в списке по имени
-    first = 0
-    last = len(array)
-    last_index = -1
-
-    while (first <= last):
-        mid = (first + last) // 2
-
-        if element == array[mid]['Name']:
-            last_index = mid
-            first = mid + 1  # Поиск вправо (более высокие индексы)
-
-        if element < array[mid]['Name']:
+        if element < array[mid][dict_key]:
             last = mid - 1
         else:
             first = mid + 1
@@ -121,44 +78,49 @@ def get_by_date(date=None, name=None, filename=None):
             list_data.append(items)
 
         if date == 'all':
+            dict_key = 'Name'
+
             list_data.sort(key=lambda name: name['Name'])  # Сортируем список по имени
 
-            index_first = binary_search_iterative_first_name(list_data,
-                                                             name)  # Индекс первого вхождения в список, передаем список и имя
-            index_last = binary_search_iterative_last_name(list_data,
-                                                           name)  # Индекс последнего вхождения в список, передаем список и имя
+            index_first = binary_search_iterative_first(list_data,
+                                                             name, dict_key)  # Индекс первого вхождения в список, передаем список и имя
+            index_last = binary_search_iterative_last(list_data,
+                                                           name, dict_key)  # Индекс последнего вхождения в список, передаем список и имя
 
             find_info = list_data[index_first:index_last]  # Выводим срез по имени и передаем в результат
 
-        if name == 'all':
+        elif name == 'all':
+            dict_key = 'date'
+
             list_data.sort(key=lambda data: data['date'])  # Сортируем список по дате
 
-            index_first = binary_search_iterative_first_data(list_data,
-                                                             date)  # Индекс первого вхождения в список, передаем список и дату
-            index_last = binary_search_iterative_last_data(list_data,
-                                                           date)  # Индекс последнего вхождения в список, передаем список и дату
+            index_first = binary_search_iterative_first(list_data,
+                                                             date, dict_key)  # Индекс первого вхождения в список, передаем список и дату
+            index_last = binary_search_iterative_last(list_data,
+                                                           date, dict_key)  # Индекс последнего вхождения в список, передаем список и дату
 
             find_info = list_data[index_first:index_last]  # Выводим срез по дате и передаем в результат
 
         else:
+            dict_key = 'date'
+
             list_data.sort(key=lambda data: data['date'])  #
 
-            index_first = binary_search_iterative_first_data(list_data,
-                                                             date)  # Индекс первого вхождения в список, передаем список и дату
-            index_last = binary_search_iterative_last_data(list_data,
-                                                           date)  # Индекс последнего вхождения в список, передаем список и дату
+            index_first = binary_search_iterative_first(list_data,
+                                                             date, dict_key)  # Индекс первого вхождения в список, передаем список и дату
+            index_last = binary_search_iterative_last(list_data,
+                                                           date, dict_key)  # Индекс последнего вхождения в список, передаем список и дату
 
             list_with_data = list_data[index_first:index_last]  # Строка из словарей по дате из найденных индексов
             index_name = LinearSearch(list_with_data,
                                       name)  # Индекс значения по имени, передаем строку из словарей и имя
 
-            find_info = list_with_data[index_name]  # Словарь из запрошенной даты и имени
+            find_info = [list_with_data[index_name]]  # Список из запрошенной даты и имени
 
         with open(data/filename, 'w', newline='') as write_file:  # Запись результата поиска в файл
-            fieldnames = ['date', 'open', 'high', 'low', 'close', 'volume', 'Name']
-            writer = csv.DictWriter(write_file, delimiter='|', fieldnames=fieldnames)
-            writer.writeheader()
-            writer.writerow(find_info)
+            writer = csv.writer(write_file, delimiter='|')
+            for item in find_info:
+                writer.writerow(item.values())
 
 
 find_data = input(f'Дата в формате yyyy-mm-dd [all]: ') or 'all'
